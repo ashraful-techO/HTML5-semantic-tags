ead1bd93f42e621bce2e351deb837bd695f31108
ashraful-techO <ashraful.islam.cse.97@gmail.com>
2025-08-18 11:32:19 +0600

    JS Basic

--- JavaScript/Basic/script.js
+++ JavaScript/Basic/script.js
@@ -1,3 +1,7 @@
 const title = document.getElementById("demo");
 
 title.innerHTML= "My First JavaScript";
+
+function myFunction(){
+    document.getElementById("peragraph").innerHTML = "Peragraph changed.";
+}
