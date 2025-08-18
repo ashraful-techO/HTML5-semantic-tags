7f3294e1e9eb7635f52687cd8f607fbb8683eb10
ashraful-techO <ashraful.islam.cse.97@gmail.com>
2025-08-18 15:20:00 +0600

    Variables

---
+++ JavaScript/Variables/script.js
@@ -0,0 +1,11 @@
+const name = "Alice";
+const name1 = "Shanto";
+console.log(`${name} name once decleared and ${name1} name1 another name`);
+
+let counter = 0;
+console.log(counter++); //at this stage counter is 0 then it will go for counter+1=0+1=1
+// cant re declear let counter = 5;
+console.log(counter); // Here counter is 1
+
+
+
